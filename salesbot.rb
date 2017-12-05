370eaf563b29fc1f3426e4c58995d428286c0266
John Fitisoff <jfitisoff@yahoo.com>
2017-12-05 14:36:43 -0800

    dunning wh

--- salesbot.rb
+++ salesbot.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'nokogiri'
 require 'json'
 require 'httparty'
+require 'active_support/all'
 
 SLACK_ENDPOINT='https://hooks.slack.com/services/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'
 INVOICES_URL='https://slackathon-sales.recurly.com/invoices/'
@@ -20,6 +21,8 @@ post '/webhook' do
   case xml.root.name
   when "closed_invoice_notification"
     closed_invoice xml
+  when 'new_dunning_event_notification'
+    dunning_event xml
   else
     post_webhook "Received webhook: #{xml.root.name}"
   end
@@ -51,4 +54,26 @@ def closed_invoice xml
   EOS
 
   post_webhook str
+end
+
+def dunning_event xml
+  invoice_number = xml.xpath('//invoice/invoice_number').text
+  company = xml.xpath('//account/company_name').text
+  account_name = xml.xpath('//account/first_name').text + ' ' + xml.xpath('//account/last_name').text
+  state = xml.xpath('//invoice/invoice_number').text
+  amount = xml.xpath('//invoice/total_in_cents').text.to_f/100
+  currency = xml.xpath('//invoice/currency').text.to_f/100
+  dunning_events_count = xml.xpath('//invoice/dunning_events_count').text
+  final_dunning_event = xml.xpath('//invoice/final_dunning_event').text
+  final_dunning_event =~ true ? final = 'No dunning attempts remaining.' : final = "Invoice is still in dunning."
+  state == 'collected' ? success = 'Successful' : success = 'Failed'
+
+  str = <<~eos
+  #{success} dunning attempt for #{company.present? ? company : name}
+  Collection attempt: #{dunning_events_count} (#{final})
+  Invoice amount: #{amount} #{currency}
+  eos
+
+  post_webhook str
+  # post_webhook "New Invoice! <#{INVOICES_URL}#{invoice_number}|\##{invoice_number}>"
 end
