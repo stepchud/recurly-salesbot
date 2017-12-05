2485a350a24b2219e58cdeb287e7705ab3547105
Stephen Chudleigh <schudleigh@recurly.com>
2017-12-05 11:46:29 -0800

    add webhook endpoint

--- salesbot.rb
+++ salesbot.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'json'
+require 'pp'
 
 get '/' do
-    "Hello World!"
+  "Recurly Salesbot at your service!"
+end
+
+get '/webhook' do
+  "Hello Webhooks"
+end
+
+post '/webhook' do
+  pp "Receiving Webhook #{params[:webhookId]}"
+  pp JSON.parse(params[:result])
 end
